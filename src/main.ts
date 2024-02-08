ff0b5da3eacc30788584ffa2805ebd42e7777ad8
macarena <macarena@blockinar.com>
2024-02-08 18:17:18 -0300

    feat: Add GoogleSheetApi and Firestore connection, add pagination

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import './assets/main.css'
-
-import { createApp } from 'vue'
+import {  firebaseApp } from './firebase'
+import { createApp } from 'vue-demi'
 import App from './App.vue'
 import router from './router'
+import { VueFire, VueFireAuth } from 'vuefire'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VueFire, {
+    firebaseApp,
+    modules: [
+        // we will see other modules later on
+        VueFireAuth(),
+      ]
+})
 
 app.mount('#app')
